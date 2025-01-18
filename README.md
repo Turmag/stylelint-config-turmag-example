e1755e272a65ab8216194b07c3f571907ab18bad
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-18 23:51:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# stylelint-config-turmag-example
+
+Turmag's example stylelint-config. For reference only. Not for use.
+
+# License
+This code is licensed under the [MIT](https://github.com/Turmag/turmag-stylelint-config/blob/main/LICENSE) License.
---
+++ index.js
@@ -0,0 +1,18 @@
+export default {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-recommended',
+        'stylelint-config-recommended-vue/scss',
+    ],
+    plugins: ['stylelint-order', '@stylistic/stylelint-plugin'],
+    rules: {
+        '@stylistic/indentation': [
+            4,
+            { baseIndentLevel: 1 },
+        ],
+        '@stylistic/no-empty-first-line': true,
+        '@stylistic/no-extra-semicolons': true,
+        'import-notation': 'string',
+        'selector-class-pattern': '[a-z]+[A-Z]*',
+    },
+};
